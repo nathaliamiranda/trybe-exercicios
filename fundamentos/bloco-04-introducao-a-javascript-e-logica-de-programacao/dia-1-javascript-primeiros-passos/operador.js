46af22c68c158b9850ecd2f7931933d0d7e9e2b1
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-08-25 16:27:01 -0300

    usando operador or

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/operador.js
@@ -0,0 +1,9 @@
+let weekDay = "sábado";
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}
+else {
+ console.log ("FINALMENTE, descando merecido UwU");
+}
+
+
