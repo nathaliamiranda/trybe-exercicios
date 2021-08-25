f56bc33cfd3cb9a335d8e20cdd8f56d10ea96bdd
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-08-25 19:43:33 -0300

    angulos do triangulo

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/angulos-do-triangulo.js
@@ -0,0 +1,15 @@
+const angulo1 = 90;
+const angulo2 = 60;
+const angulo3 = 30;
+let soma = angulo1 + angulo2 + angulo3;
+
+if (soma===180){
+console.log (true);
+}
+else {
+console.log (false);
+}
+
+
+
+
