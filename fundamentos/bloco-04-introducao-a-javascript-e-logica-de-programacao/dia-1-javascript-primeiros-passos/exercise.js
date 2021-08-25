88ef11646f2aab65368d05d5154ea2d3ddc8663f
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-08-25 19:00:34 -0300

    faz operacao aritmetica

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercise.js
@@ -0,0 +1,15 @@
+const a = 20;
+const b= 10;
+let soma = a + b;
+console.log (soma);
+let subtração = a - b;
+console.log (subtração);
+let multiplicação = a * b;
+console.log (multiplicação);
+let divisao = a / b;
+console.log (divisao);
+let modulo = a % b;
+console.log (modulo);
+
+
+
