a74d85d8918e6fcb9afb6895d65469e6da56c42b
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-08-26 13:19:46 -0300

    modificacoes no if

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,9 +1,9 @@
 const firstNumber = 10;
 const secondNumber = 19;
 const thirdNumber = 27;
+let isEven = false;
+
 if (firstNumber % 2 ===0 || secondNumber % 2 === 0 || thirdNumber % 2 === 0) {
-console.log(true);
-}
-else {
-    console.log (false);
-}
+    isEven = true;
+};
+  console.log (isEven);
--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/true-false.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/true-false.js
@@ -1,11 +1,8 @@
 const firstNumber = 11;
 const secondNumber = 15;
 const thirdNumber = 37;
-
+let isOdd = false;
 if (firstNumber % 2 > 0|| secondNumber % 2 > 0|| thirdNumber % 2 > 0) {
-console.log(true);
-}
-else {
-    console.log (false);
-
-}
+isOdd = true;
+};
+console.log (isOdd);
