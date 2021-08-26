e5afcb102d80d9078cf7d79a78af5abd39fedb60
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-08-26 13:14:42 -0300

    usa false e true como resposta ao comparar numeros

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,9 @@
+const firstNumber = 10;
+const secondNumber = 19;
+const thirdNumber = 27;
+if (firstNumber % 2 ===0 || secondNumber % 2 === 0 || thirdNumber % 2 === 0) {
+console.log(true);
+}
+else {
+    console.log (false);
+}
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/true-false.js
@@ -0,0 +1,11 @@
+const firstNumber = 11;
+const secondNumber = 15;
+const thirdNumber = 37;
+
+if (firstNumber % 2 > 0|| secondNumber % 2 > 0|| thirdNumber % 2 > 0) {
+console.log(true);
+}
+else {
+    console.log (false);
+
+}
