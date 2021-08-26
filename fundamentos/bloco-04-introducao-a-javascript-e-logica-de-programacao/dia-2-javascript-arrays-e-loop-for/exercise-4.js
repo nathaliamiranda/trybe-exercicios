c16ec27aa00025a6bdbe7cad8d248ebca93fe07f
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-08-26 16:14:41 -0300

    imprime string dizendo valor maior ou menor que 20

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-arrays-e-loop-for/exercise-4.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+for (index = 0; index < numbers.length; index+=1) {
+    result += numbers[index];
+    }
+let media = (result / 10);
+ console.log(media);
+
+ if (media>20) {
+     console.log ("Valor maior que 20");
+ } else {
+     console.log ("Valor menor que 20");
+ }
+ 
