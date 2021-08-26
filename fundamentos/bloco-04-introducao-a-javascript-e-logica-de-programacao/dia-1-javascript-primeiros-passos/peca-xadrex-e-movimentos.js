09e234fc1ba10ed8342aed097c97f71e9557e7e3
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-08-25 22:40:06 -0300

    usa switch case para descobrir peças de xadrez

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/peca-xadrex-e-movimentos.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/peca-xadrex-e-movimentos.js
@@ -1,22 +1,22 @@
-const peca = "bispo";
+const peca = "CAVALO";
 switch (peca.toLowerCase()) {
  case 'rei': 
-    console.log('rei - anda só em uma direção e apenas uma casa')
+    console.log('Rei - anda só em uma direção e apenas uma casa')
     break;
  case 'rainha': 
  console.log('Diagonal, horizontal e vertical')
  break;
  case 'torre':
-    console.log('Torre -> Horizontal e vertical.');
+    console.log('Torre - Horizontal e vertical.');
     break;
  case 'bispo':
-    console.log('Bispo-> Diagonal.');
+    console.log('Bispo - Diagonal.');
     break;
     case 'peão':
-    console.log("Peão -> Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+    console.log("Peão - Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
     break;
   case 'cavalo':
-    console.log('Cavalo -> "L" pode pular sobre as peças.');
+    console.log('Cavalo - anda em "L" podendo pular as peças');
     break
   default:
     console.log('Erro, peça inválida!');
