9a3e35e4d096a64ee6f9669938617238dd7491ee
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-08-27 15:16:57 -0300

    faz quadrado de asteristicos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/exercise-2.js
@@ -0,0 +1,9 @@
+let n = 7;
+let asteristico = "*";
+let entrada = ""
+for (let index =0; index < n; index+=1) {
+    entrada = entrada + asteristico;
+};
+for (let index = 0; index < n; index+=1) {
+    console.log (entrada);
+}
