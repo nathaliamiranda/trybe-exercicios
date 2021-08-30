9717697a72b1170e7d39648a9037fe653a0fa67e
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-08-30 12:36:05 -0300

    alteracoes no if e else

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/exercises-6.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/exercises-6.js
@@ -5,5 +5,7 @@ for (let numero = 2; numero <= numeroTestado; numero += 1) {
   if (numeroTestado % numero === 0) divisor += 1;
 }
 
-if (divisor === 2) console.log(numeroTestado + ' é primo');
-else console.log(numeroTestado + ' não é primo');
+if (divisor === 2) {
+  console.log(numeroTestado + ' é primo');
+} else {
+  console.log(numeroTestado + ' não é primo'); };
