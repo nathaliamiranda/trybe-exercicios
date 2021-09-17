311c7d72a58806bafe2111485ef059bc88a80c90
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-09-17 00:36:24 -0300

    modifica css

--- fundamentos/bloco-06-html-e-css-forms/dia-1-html-e-css-forms/script.js
+++ fundamentos/bloco-06-html-e-css-forms/dia-1-html-e-css-forms/script.js
@@ -91,25 +91,23 @@ window.onload = function () {
   buttonApagar.addEventListener('click', limparButton);
 }
 
-// parse(dateString, format = 'YYYY-MM-DD')
-
-// let picker = new Pikaday({
-//     field: document.getElementById('datepicker'),
-//     format: 'D/M/YYYY',
-//     toString(date, format) {
-//         // you should do formatting based on the passed format,
-//         // but we will just return 'D/M/YYYY' for simplicity
-//         const day = date.getDate();
-//         const month = date.getMonth() + 1;
-//         const year = date.getFullYear();
-//         return `${day}/${month}/${year}`;
-//     },
-//     parse(dateString, format) {
-//         // dateString is the result of `toString` method
-//         const parts = dateString.split('/');
-//         const day = parseInt(parts[0], 10);
-//         const month = parseInt(parts[1], 10) - 1;
-//         const year = parseInt(parts[2], 10);
-//         return new Date(year, month, day);
-//     }
-// });
+let picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  format: 'D/M/YYYY',
+  toString(date, format) {
+  // you should do formatting based on the passed format,
+  // but we will just return 'D/M/YYYY' for simplicity
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+  // dateString is the result of `toString` method
+  const parts = dateString.split('/');
+  const day = parseInt(parts[0], 10);
+  const month = parseInt(parts[1], 10) - 1;
+  const year = parseInt(parts[2], 10);
+  return new Date(day, month, year);
+  }
+  });
