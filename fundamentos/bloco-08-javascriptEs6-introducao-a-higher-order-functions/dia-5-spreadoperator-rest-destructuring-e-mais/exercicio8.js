4dd60e37caa6c34e750256c19cba9c3d08b7c960
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-10-08 15:31:47 -0300

    passo 8

--- fundamentos/bloco-08-javascriptEs6-introducao-a-higher-order-functions/dia-5-spreadoperator-rest-destructuring-e-mais/exercicio8.js
+++ fundamentos/bloco-08-javascriptEs6-introducao-a-higher-order-functions/dia-5-spreadoperator-rest-destructuring-e-mais/exercicio8.js
@@ -2,6 +2,8 @@
 // Dica: use default params .
 // escreva greet abaixo
 
+const greet = (name = 'usuário', greet = 'Hi') => `${greet}, ${name}`
 console.log(greet('John')) // 'Hi John'
 console.log(greet('John', 'Good morning')) // 'Good morning John'
 console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
+console.log(greet());
