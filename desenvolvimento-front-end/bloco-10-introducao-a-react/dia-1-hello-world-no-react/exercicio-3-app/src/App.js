898664d7a371721de6793769fff85a4a46ae643e
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-10-27 21:45:32 -0300

    exercicio 2 e 3

---
+++ desenvolvimento-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-3-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Header.jsx';
+import Content from './Content';
+import Footer from './Footer';
+function App() {
+  return (<>
+  <Header/>
+  <Content/>
+  <Footer/>
+  </>);
+};
+
+export default App;
