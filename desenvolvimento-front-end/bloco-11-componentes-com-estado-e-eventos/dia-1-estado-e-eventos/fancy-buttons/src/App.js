ac7327d90be07cb25a008c23c15a6930a03228ff
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-11-03 10:06:26 -0300

    fixacao 3 e 4

--- desenvolvimento-front-end/bloco-11-componentes-com-estado-e-eventos/dia-1-estado-e-eventos/fancy-buttons/src/App.js
+++ desenvolvimento-front-end/bloco-11-componentes-com-estado-e-eventos/dia-1-estado-e-eventos/fancy-buttons/src/App.js
@@ -5,27 +5,35 @@ import './App.css';
 não é a maneira correta de se criar eventos
 em React! A função se refere ao componente,
 então deve ser parte de sua classe! */
-function handleClick() {
-  console.log('Clicou no botão!')
-}
-function handleClickTwo() {
-  console.log('Clique aqui!')
-}
-function handleClickTree() {
-  console.log('Clique!')
-}
 
 
 class App extends React.Component {
-  /* Repare que, diferentemente do HTML, no
-  JSX você associa uma função a um evento
-  passando a própria função entre chaves `{}` */
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClickTwo = this.handleClickTwo.bind(this)
+    this.handleClickTree = this.handleClickTree.bind(this) 
+  }
+
+   handleClick() {
+     console.log(this)
+    console.log('Clicou no botão!')
+  }
+   handleClickTwo() {
+     console.log(this)
+    console.log('Clique aqui!')
+  }
+   handleClickTree() {
+     console.log(this);
+    console.log('Clique!')
+  }
+
   render() {
     return (
     <div>
-     <button onClick={handleClick}>Meu botão</button>
-     <button onClick={handleClickTwo}>Meu botão 2</button>
-     <button onClick={handleClickTree}>Meu botão 3</button>
+     <button onClick={this.handleClick}>Meu botão</button>
+     <button onClick={this.handleClickTwo}>Meu botão 2</button>
+     <button onClick={this.handleClickTree}>Meu botão 3</button>
     </div>
     );
   }
