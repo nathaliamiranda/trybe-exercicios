ff63090de3a9cea3ee22101406497928c532ad3b
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-11-03 11:35:17 -0300

    exercicio 8 e 9

--- desenvolvimento-front-end/bloco-11-componentes-com-estado-e-eventos/dia-1-estado-e-eventos/fancy-buttons/src/App.js
+++ desenvolvimento-front-end/bloco-11-componentes-com-estado-e-eventos/dia-1-estado-e-eventos/fancy-buttons/src/App.js
@@ -6,6 +6,9 @@ import './App.css';
 // 5 - Agora você vai converter sua aplicação para uma que conta o número de cliques dado em cada botão! Primeiramente, defina um estado inicial para a contagem de cada botão.
 // 6 - Agora, quando um botão for clicado, altere de forma assíncrona o estado deste botão de zero para um.
 // 7 - Por fim, baseie-se no estado anterior ao atual para incrementar a contagem de cliques cada vez que um botão for clicado!
+// 8 - Defina uma lógica que estabeleça que, quando o número de cliques no botão for par, ele deve ser verde.
+// 9 - A cor atual do botão deve ser impressa num console.log() de dentro da função do evento que lida com o clique. Faça isso acontecer!
+// 🦜 Dica: Lembre-se de substituir a referência à função, no evento, por uma callback que invoca!
 
 
 
@@ -41,16 +44,33 @@ class App extends React.Component {
     console.log('Clique!')
   }
 
-  render() {
-    return (
-    <div>
-     <button onClick={this.handleClick}>{this.state.numeroDeCliques1}</button>
-     <button onClick={this.handleClickTwo}>{this.state.numeroDeCliques2}</button>
-     <button onClick={this.handleClickTree}>{this.state.numeroDeCliques3}</button>
-    </div>
-    );
-  }
-}
+      // Para essa função, não precisamos utilizar o bind porque ela é utilizada
+    // apenas dentro do contexto do componente App
+    getButtonColor(number) {
+      return number % 2 === 0 ? 'green' : 'white';
+    }
 
+//   render() {
+//     return (
+//     <div>
+//      <button onClick={this.handleClick}>Botão 1 | Count = {this.state.numeroDeCliques1}</button>
+//      <button onClick={this.handleClickTwo}>Botão 2 | Count = {this.state.numeroDeCliques2}</button>
+//      <button onClick={this.handleClickTree}>Botão 3 | Count = {this.state.numeroDeCliques3}</button>
+//     </div>
+//     );
+//   }
+// }
+
+render() {
+  const { numeroDeCliques1, numeroDeCliques2, numeroDeCliques3 } = this.state;
+  return (
+  <div>
+   <button onClick={this.handleClick} style={{ backgroundColor: this.getButtonColor(numeroDeCliques1)}}>Botão 1 | Count = { numeroDeCliques1 }</button>
+   <button onClick={this.handleClickTwo} style={{ backgroundColor: this.getButtonColor(numeroDeCliques2)}}>Botão 2 | Count = { numeroDeCliques2 }</button>
+   <button onClick={this.handleClickTree} style={{ backgroundColor: this.getButtonColor(numeroDeCliques3)}}>Botão 3 | Count = { numeroDeCliques3 }</button>
+  </div>
+  );
+}
+}
 export default App;
 
