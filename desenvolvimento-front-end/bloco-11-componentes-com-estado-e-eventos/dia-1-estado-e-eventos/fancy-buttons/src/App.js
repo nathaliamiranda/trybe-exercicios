94fad141c2043393c167e2bb5133faa39d8c7c74
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-11-03 10:58:55 -0300

    fixacao parte 5, 6 e 7

--- desenvolvimento-front-end/bloco-11-componentes-com-estado-e-eventos/dia-1-estado-e-eventos/fancy-buttons/src/App.js
+++ desenvolvimento-front-end/bloco-11-componentes-com-estado-e-eventos/dia-1-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import './App.css';
 
-/* Embora isso funcione, essa DEFINITIVAMENTE
-não é a maneira correta de se criar eventos
-em React! A função se refere ao componente,
-então deve ser parte de sua classe! */
+// 3 - Declare dentro da classe do seu componente dos exercícios de fixação acima a função que lida com o evento que antes era lidado por uma função do lado de fora da classe!
+// 4 - Garanta acesso ao objeto this na função que você declarou.
+// 5 - Agora você vai converter sua aplicação para uma que conta o número de cliques dado em cada botão! Primeiramente, defina um estado inicial para a contagem de cada botão.
+// 6 - Agora, quando um botão for clicado, altere de forma assíncrona o estado deste botão de zero para um.
+// 7 - Por fim, baseie-se no estado anterior ao atual para incrementar a contagem de cliques cada vez que um botão for clicado!
+
 
 
 class App extends React.Component {
   constructor() {
-    super()
-    this.handleClick = this.handleClick.bind(this)
-    this.handleClickTwo = this.handleClickTwo.bind(this)
-    this.handleClickTree = this.handleClickTree.bind(this) 
+    super();
+
+    this.state = {
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0,
+    };
+    
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClickTwo = this.handleClickTwo.bind(this);
+    this.handleClickTree = this.handleClickTree.bind(this); 
   }
 
    handleClick() {
+     this.setState((estadoAnterior, _props) => ({numeroDeCliques1: estadoAnterior.numeroDeCliques1 +1 }))
      console.log(this)
     console.log('Clicou no botão!')
   }
    handleClickTwo() {
+     this.setState((estadoAnterior, _props) => ({numeroDeCliques2: estadoAnterior.numeroDeCliques2 +1}))
      console.log(this)
     console.log('Clique aqui!')
   }
    handleClickTree() {
+     this.setState((estadoAnterior, _props) => ({numeroDeCliques3: estadoAnterior.numeroDeCliques3 +1}))
      console.log(this);
     console.log('Clique!')
   }
@@ -31,9 +44,9 @@ class App extends React.Component {
   render() {
     return (
     <div>
-     <button onClick={this.handleClick}>Meu botão</button>
-     <button onClick={this.handleClickTwo}>Meu botão 2</button>
-     <button onClick={this.handleClickTree}>Meu botão 3</button>
+     <button onClick={this.handleClick}>{this.state.numeroDeCliques1}</button>
+     <button onClick={this.handleClickTwo}>{this.state.numeroDeCliques2}</button>
+     <button onClick={this.handleClickTree}>{this.state.numeroDeCliques3}</button>
     </div>
     );
   }
