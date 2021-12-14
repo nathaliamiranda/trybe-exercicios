a76f400e09553106ca9af5a49b73bfe23a6dd7a9
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-12-14 12:17:22 -0300

    acerto de linhas

--- desenvolvimento-front-end/bloco-14-testes-automatizados-com-Rect-testing-library/dia-2-mocks-e-inputs/exercicio1/src/service.js
+++ desenvolvimento-front-end/bloco-14-testes-automatizados-com-Rect-testing-library/dia-2-mocks-e-inputs/exercicio1/src/service.js
@@ -4,12 +4,20 @@ const upperCase = (string) => string.toUpperCase();
 const firstLetra = (string) => string.charAt(0);
 const concatString = (string1, string2 ) => string1.concat(string2);
 
+const dogsAPI = () => {
+  return fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json()
+    .then(json => response.ok ? Promise.resolve(json) : Promise.reject(json)
+    )
+);
+};
 
 module.exports = { 
   randomNumber, 
   upperCase, 
   firstLetra, 
-  concatString 
+  concatString,
+  dogsAPI,
 };
 
 
--- desenvolvimento-front-end/bloco-14-testes-automatizados-com-Rect-testing-library/dia-2-mocks-e-inputs/exercicio1/src/service.test.js
+++ desenvolvimento-front-end/bloco-14-testes-automatizados-com-Rect-testing-library/dia-2-mocks-e-inputs/exercicio1/src/service.test.js
@@ -11,7 +11,6 @@ test('Teste se a função é chamada e quantas vezes é chamada', () => {
   expect(service.randomNumber).toHaveBeenCalledTimes(2);
 })
 
-
 test('Crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez.', () => {
   service.randomNumber.mockImplementationOnce((a, b) => a / b);
   
@@ -22,7 +21,6 @@ test('Crie uma nova implementação, que deve receber dois parâmetros e retorna
 });
 
 describe('Requesito 3 - testando implementações' , () => {
-
   test('Utilize o mock e desenvolva uma nova implementação que receba três parâmetros. Retorne a multiplicação dos parâmetros', () => {
     service.randomNumber.mockImplementationOnce((a, b, c) => a * b * c);
     
@@ -47,7 +45,6 @@ describe('Requesito 3 - testando implementações' , () => {
 });
 
 describe('Requesito 4 - A - Desenvolva uma nova implementação', () => {
-
   test('Requesito 4 - mock da função para receber um parâmetro e retornar em lowercase', ()=> {
     service.upperCase.mockImplementation((a) => a.toLowerCase());
   
@@ -56,7 +53,6 @@ describe('Requesito 4 - A - Desenvolva uma nova implementação', () => {
     service.upperCase.mockRestore();
 
     expect(service.upperCase('nathalia')).toBe('NATHALIA');
-  
   })
   
   test('Requesito 4 - mock da função que recebe um parâmetro e retorna a última letra' , () => {
@@ -64,13 +60,34 @@ describe('Requesito 4 - A - Desenvolva uma nova implementação', () => {
     service.firstLetra.mockImplementation((a) => a[a.length -1])
   
     expect(service.firstLetra('meninas')).toBe('s');
+})
 
-  })
   test('Requisito 4 - recebe 3 parâmetros e os concatena' , () => {
     service.concatString.mockImplementation((a, b, c) => a.concat(b, c));
   
     expect(service.concatString('es', 'ca', 'da')).toBe('escada');
-  
   })
-
 })
+
+describe("Requisito 5 - testando a requisição", () => {
+  service.dogsAPI = jest.fn();
+  afterEach(service.dogsAPI.mockReset);
+
+  test("testando requisição caso a promisse resolva", async () => {
+    service.dogsAPI.mockResolvedValue("request sucess");
+
+    service.dogsAPI();
+    expect(service.dogsAPI).toHaveBeenCalled();
+    expect(service.dogsAPI).toHaveBeenCalledTimes(1);
+    await expect(service.dogsAPI()).resolves.toBe("request sucess");
+    expect(service.dogsAPI).toHaveBeenCalledTimes(2);
+  });
+
+  test("testando requisição caso a promisse seja rejeitada", async () => {
+    service.dogsAPI.mockRejectedValue("request failed");
+
+    expect(service.dogsAPI).toHaveBeenCalledTimes(0);
+    await expect(service.dogsAPI()).rejects.toMatch("request failed");
+    expect(service.dogsAPI).toHaveBeenCalledTimes(1);
+  });
+});
