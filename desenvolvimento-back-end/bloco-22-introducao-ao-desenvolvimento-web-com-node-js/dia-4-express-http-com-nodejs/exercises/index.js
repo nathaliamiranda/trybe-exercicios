710505b3eee1d60a6d598adc04754e18605a2c98
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-03-16 21:02:27 -0300

    exercicio 4

--- desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-express-http-com-nodejs/exercises/index.js
+++ desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-express-http-com-nodejs/exercises/index.js
@@ -5,24 +5,32 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/ping', (_req, res) => res.json( { "message": 'pong' }))
+app.get('/ping', (_req, res) => res.json({ "message": 'pong' }))
 
 app.post('/hello', (req, res) => {
-    const { name } = req.body;
-    res.status(200).json({ "message": `Hello, ${name}!` })
-  })
-
-app.post('/greeting', (req, res) => {
-    const { name, age } = req.body;
-    
-    if(parseInt(age, 10) <= 17) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-    res.status(200).json({ message: `Hello, ${name}!` })
+  const { name } = req.body;
+  res.status(200).json({ "message": `Hello, ${name}!` })
 })
 
-app.use( function (err, _req, res, _next) {
-    res.status(500).send(`Something bad happened! Message: ${err.message}`);
+app.post('/greeting', (req, res) => {
+  const { name, age } = req.body;
+
+    if (parseInt(age, 10) <= 17) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+  res.status(200).json({ message: `Hello, ${name}!` })
+})
+
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+     
+  res.status(200).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade`})
+})
+
+
+app.use(function (err, _req, res, _next) {
+  res.status(500).send(`Something bad happened! Message: ${err.message}`);
 });
 
 app.listen(3000, () => console.log('listening on port 3000'));
