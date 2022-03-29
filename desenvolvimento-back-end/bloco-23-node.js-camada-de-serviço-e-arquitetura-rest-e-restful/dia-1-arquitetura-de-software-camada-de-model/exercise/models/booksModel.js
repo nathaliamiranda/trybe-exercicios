5dc61d36cc468c40a1ed1d4c953c663b9868376c
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-03-29 09:38:14 -0300

    Feat: exercise 2 complete

--- desenvolvimento-back-end/bloco-23-node.js-camada-de-serviço-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercise/models/booksModel.js
+++ desenvolvimento-back-end/bloco-23-node.js-camada-de-serviço-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercise/models/booksModel.js
@@ -1,5 +1,6 @@
 const connection = require('./connection');
 
+
 const getAll = async () => {
   const [books] = await connection.execute('SELECT * FROM model_example.books;');
 
@@ -10,6 +11,34 @@ const getAll = async () => {
   }));
 };
 
+const getByAuthorId = async (authorId) => {
+  const query = 'SELECT * FROM model_example.books WHERE author_id=?;'
+  const [books] = await connection.execute(query, [authorId]);
+
+  return books.map(({ id, title, author_id }) => ({
+    id,
+    title,
+    authorId: author_id,
+  }));
+};
+
+const getById = async (id) => {
+  const query = 'SELECT * FROM model_example.books WHERE id=?;'
+
+  const [books] = await connection.execute(query, [id]);
+
+  if (books.length === 0) return null;
+
+  return books.map(({ id, title, author_id }) => ({
+    id,
+    title,
+    authorId: author_id,
+  }))[0];
+}
+
 module.exports = {
   getAll,
+  getByAuthorId,
+  getById
 };
+
