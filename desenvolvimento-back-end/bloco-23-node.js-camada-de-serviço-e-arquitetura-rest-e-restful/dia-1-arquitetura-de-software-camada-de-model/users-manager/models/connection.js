1faf5fd934401fc5dc30fe33edf8be9239853918
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-03-29 10:29:02 -0300

    Feat: exercise 3 complete

---
+++ desenvolvimento-back-end/bloco-23-node.js-camada-de-serviço-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/users-manager/models/connection.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST, 
+  user: process.env.MYSQL_USER, 
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB_NAME });
+
+module.exports = connection;
