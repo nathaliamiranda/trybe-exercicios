475bcf5cf0828fcf86ef89d522174b78b1dda2e3
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-04-07 11:21:42 -0300

    Feat: populate table  with seeders

--- desenvolvimento-back-end/bloco-24-node-js-ORM-e-autenticacao/dia-1-ORM-interface-da-aplicacao-com-o-banco-de-dados/books-api/seeders/20220407133844-books.js
+++ desenvolvimento-back-end/bloco-24-node-js-ORM-e-autenticacao/dia-1-ORM-interface-da-aplicacao-com-o-banco-de-dados/books-api/seeders/20220407133844-books.js
@@ -1,24 +1,33 @@
-'use strict';
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkInsert('Books',
+      [
+        {
+          title: 'Christine',
+          author: 'Stephen King',
+          pageQuantity: 616,
+          createdAt: new Date("2022-01-28T13:23:32.514Z"),
+          updatedAt: new Date("2022-01-28T13:23:32.514Z"),
+        },
+        {
+          title: 'Apanhador de Sonhos',
+          author: 'Stephen King',
+          pageQuantity: 656,
+          createdAt: new Date("2022-01-28T04:51:32.514Z"),
+          updatedAt: new Date("2022-01-28T04:51:32.514Z"),
+        },
+        {
+          title: 'A Dança da Morte',
+          author: 'Stephen King',
+          pageQuantity: '1248',
+          createdAt: new Date("2022-01-28T19:37:32.514Z"),
+          updatedAt: new Date("2022-01-28T19:37:32.514Z"),
+        }
+      ],
+      {},
+    );
   },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkDelete('Books', null, {});
+  },
 };
