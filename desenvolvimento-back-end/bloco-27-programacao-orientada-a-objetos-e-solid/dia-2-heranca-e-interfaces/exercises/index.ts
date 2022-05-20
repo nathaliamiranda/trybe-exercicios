83d577650791427f6684c892997f44f0c8c5e577
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-20 11:04:27 -0300

    create Person class

---
+++ desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-2-heranca-e-interfaces/exercises/index.ts
@@ -0,0 +1,7 @@
+import Person from './Person';
+
+const laura = new Person('Laura Oliveira', new Date('1980/01/25'));
+const theo = new Person('Theo Andrade', new Date('2005/10/02'));
+
+console.log(laura);
+console.log(theo);
