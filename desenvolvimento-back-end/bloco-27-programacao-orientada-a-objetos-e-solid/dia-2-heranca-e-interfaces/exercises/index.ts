7e4a868af9f22a3e4593603d8b94f179e8d7447c
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-20 11:24:34 -0300

    feat: create Student class

--- desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-2-heranca-e-interfaces/exercises/index.ts
+++ desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-2-heranca-e-interfaces/exercises/index.ts
@@ -1,7 +1,29 @@
 import Person from './Person';
+import Student from './Student';
 
 const laura = new Person('Laura Oliveira', new Date('1980/01/25'));
 const theo = new Person('Theo Andrade', new Date('2005/10/02'));
 
 console.log(laura);
 console.log(theo);
+
+const maria = new Student('Maria Simões', new Date('2007/04/27'));
+const marcio = new Student('Marcio Oliveira', new Date('2008/05/16'));
+const joyce = new Student('Joyce Carvalho', new Date('2009/08/03'));
+const bruna = new Student('Bruna Passos', new Date('2006/11/07'));
+
+maria.examsGrades = [10, 8, 7, 8];
+marcio.examsGrades = [7, 6, 7, 8];
+joyce.examsGrades = [7, 8, 9, 8];
+bruna.examsGrades = [7, 7, 9, 8];
+
+maria.worksGrades = [10, 8];
+marcio.worksGrades = [8, 9];
+joyce.worksGrades = [7, 8];
+bruna.worksGrades = [10, 7];
+
+
+console.log(maria);
+console.log(marcio);
+console.log(joyce);
+console.log(bruna);
