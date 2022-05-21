8d439e7d54b6cdf74bdc97064662e87342a6fb88
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-21 19:26:20 -0300

    feat: create Teacher class and object

--- desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-3-polimorfismo/exercises/index.ts
+++ desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-3-polimorfismo/exercises/index.ts
@@ -1,4 +1,11 @@
 import Student from './Student';
+import Subject from './Subject';
+import Teacher from './Teacher';
 
 const danielle = new Student('Danielle Cherem', new Date('2005/03/17'));
 console.log(danielle);
+
+const math = new Subject('Matemática');
+
+const sarah = new Teacher('Sarah Watson', new Date('1995/01/27'), 4000, math);
+console.log(sarah);
