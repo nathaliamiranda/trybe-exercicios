82de2a08565bef2b1b7f4ad66d11cadb6cafe9cb
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-22 14:15:46 -0300

    feat: create objects

--- desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-3-polimorfismo/exercises/index.ts
+++ desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-3-polimorfismo/exercises/index.ts
@@ -1,11 +1,13 @@
 import EvaluationResult from './EvaluationResult';
 import Exam from './Exam';
+import Order from './Order';
+import OrderItem from './OrderItem';
 import Student from './Student';
 import Subject from './Subject';
 import Teacher from './Teacher';
 import Work from './Work';
 
-const danielle = new Student('Danielle Cherem', new Date('2008/0/17'));
+const danielle = new Student('Danielle Cherem', new Date('2008/08/17'));
 console.log(danielle);
 
 const math = new Subject('Matemática');
@@ -18,3 +20,11 @@ const workMath = new Work(sarah, 50);
 
 danielle.addEvaluationResult(new EvaluationResult(examMath, 8));
 danielle.addEvaluationResult(new EvaluationResult(workMath, 9));
+
+const sandwiche = new OrderItem('Sanduiche Robife', 40.00);
+const juice = new OrderItem('Suco de Laranja', 11.00);
+const dessert = new OrderItem('Brownie', 8.50);
+
+const danielleOrder = new Order(danielle, [sandwiche, juice, dessert], 'cartão', 0.50);
+
+console.log('Pedido da Danielle: ', danielleOrder);
