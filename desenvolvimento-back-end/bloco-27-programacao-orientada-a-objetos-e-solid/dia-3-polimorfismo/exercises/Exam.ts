42abe793b60aa196a3c0fcfc41a52909c7f8ac2f
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-22 13:50:46 -0300

    feat: create Exam class

---
+++ desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-3-polimorfismo/exercises/Exam.ts
@@ -0,0 +1,16 @@
+import Teacher from './Teacher';
+import Evaluation from './Evaluation';
+
+export default class Exam extends Evaluation {
+  constructor(teacher: Teacher, score: number) {
+    super(teacher, score);
+  }
+
+  set score(value: number) {
+    if (value > 11) {
+      throw new Error('A pontução deve ser menor que 11 pontos.');
+    }
+
+    super.score = value;
+  }
+}
