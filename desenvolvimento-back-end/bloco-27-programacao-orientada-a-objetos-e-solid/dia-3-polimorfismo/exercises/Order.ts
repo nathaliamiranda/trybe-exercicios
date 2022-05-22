57304adc6ccdebd6ff186ad75611233336baac13
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-05-22 14:10:38 -0300

    feat: create Order class

---
+++ desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-3-polimorfismo/exercises/Order.ts
@@ -0,0 +1,66 @@
+import OrderItem from './OrderItem';
+import Person from './Person';
+
+export default class Order {
+  private _client: Person;
+  private _items: OrderItem[] = [];
+  private _discount = 0;
+
+  constructor(client: Person, items: OrderItem[], private _paymentMethod: string, discount?: number) {
+    this._client = client;
+    this.items = items;
+    this.paymentMethod = _paymentMethod;
+
+    if (discount) this.discount = discount;
+  }
+
+  get client(): Person {
+    return this._client;
+  }
+
+  set client(value: Person) {
+    this._client = value;
+  }
+
+  get items(): OrderItem[] {
+    return this._items;
+  }
+
+  set items(value: OrderItem[]) {
+    if (value.length === 0) {
+      throw new Error('O pedido deve ter pelo meno um item.');
+    }
+    this._items = value;
+  }
+
+  get paymentMethod(): string {
+    return this._paymentMethod;
+  }
+
+  set paymentMethod(value: string) {
+    if (!['dinheiro', 'cartão', 'vale'].includes(value)) {
+      throw new Error('A forma de pagamento não é válida.');
+    }
+    this._paymentMethod = value;
+  }
+
+  get discount(): number {
+    return this._discount;
+  }
+
+  set discount(value: number) {
+    if (value < 0) {
+      throw new Error('O desconto não pode ser um valor negativo.');
+    }
+    this._discount = value;
+  }
+
+  calculateTotal(): number {
+    return this.items
+      .reduce((previousValue, item) => previousValue + item.price, 0);
+  }
+
+  calculateTotalWithDiscount(): number {
+    return this.calculateTotal() * (1 - this.discount);
+  }
+}
